6ef0762233441c46e85a3669ccf2c3453e8e1d40
Thomas Drebes <38798057+tdrebes@users.noreply.github.com>
2023-02-26 22:52:15 +0100

    Migrated to pytest

--- test_messages.py
+++ test_messages.py
@@ -1,7 +1,6 @@
-import unittest
 import irc_bot
 
-class TestReceiveData(unittest.TestCase):
+class TestReceiveMessages():
     def test_read_messages(self):
         input = ':user1!user1@user1.tmi.twitch.tv PRIVMSG #channel :FeelsBadMan just throw it already..\r\n:user2!user2@user2.tmi.twitch.tv PRIVMSG #channel :forsenJAM \U000e0000\r\n:user3!user3@user3.tmi.twitch.tv PRIVMSG #channel :Clueless  TeaTime hmm...\r\n:user4!user4@user4.tmi.twitch.tv PRIVMSG #channel :forsenLaughingAtYou THROW forsenLaughingAtYou THROW forsenLaughingAtYou THROW\r\n'
         expectedParts = [
@@ -12,8 +11,8 @@ class TestReceiveData(unittest.TestCase):
         ]
 
         r1, r2 = irc_bot.read_messages(input, '')
-        self.assertEqual(r1, expectedParts)
-        self.assertEqual(r2, '')
+        assert r1 == expectedParts
+        assert r2 == ''
 
     def test_read_messages_results_in_overflow(self):
         input = ':user1!user1@user1.tmi.twitch.tv PRIVMSG #channel :FeelsBadMan just throw it already..\r\n:user2!user2@user2.tmi.twitch.tv PRIVMSG #channel :forsenJAM \U000e0000\r\n:user3!user3@user3.tmi.twitch.tv PRIVMSG #channel :Clueless  TeaTime hmm...\r\n:user4!user4@user4.tmi.twitch.tv PRIVMSG #channel :forsenLaughingAtYou THROW forsenLaughingAtYou THROW fors'
@@ -24,8 +23,9 @@ class TestReceiveData(unittest.TestCase):
         ]
         
         r1, r2 = irc_bot.read_messages(input, '')
-        self.assertEqual(r1, expectedParts)
-        self.assertEqual(r2, ':user4!user4@user4.tmi.twitch.tv PRIVMSG #channel :forsenLaughingAtYou THROW forsenLaughingAtYou THROW fors')
+
+        assert r1 == expectedParts
+        assert r2 == ':user4!user4@user4.tmi.twitch.tv PRIVMSG #channel :forsenLaughingAtYou THROW forsenLaughingAtYou THROW fors'
 
     def test_read_messages_with_existing_overflow(self):
         input = 'BadMan just throw it already..\r\n:user2!user2@user2.tmi.twitch.tv PRIVMSG #channel :forsenJAM \U000e0000\r\n:user3!user3@user3.tmi.twitch.tv PRIVMSG #channel :Clueless  TeaTime hmm...\r\n:user4!user4@user4.tmi.twitch.tv PRIVMSG #channel :forsenLaughingAtYou THROW forsenLaughingAtYou THROW forsenLaughingAtYou THROW\r\n'
@@ -37,8 +37,5 @@ class TestReceiveData(unittest.TestCase):
         ]
 
         r1, r2 = irc_bot.read_messages(input, ':user1!user1@user1.tmi.twitch.tv PRIVMSG #channel :Feels')
-        self.assertEqual(r1, expectedParts)
-        self.assertEqual(r2, '')
-
-if __name__ == '__main__':
-    unittest.main()
+        assert r1 == expectedParts
+        assert r2 == ''
